83f9f9ca01f72e75861c57c8a968b2210c39d2cd
Andrey Kylish <hitekdada@gmail.com>
2018-12-16 21:50:44 +0200

    N_1 - Add bot, token, username.

---
+++ src/main/java/test/bot/TestBot.java
@@ -0,0 +1,34 @@
+package test.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TestBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+// We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText() + " FROM BOT");
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "HITEK_TEST_BOT";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "534762565:AAFo70xBCRJrMk_Um2CH0qisydUA2gZ0Zks";
+    }
+}
---
+++ src/main/java/test/core/EndPoint.java
@@ -0,0 +1,19 @@
+package test.core;
+
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import test.bot.TestBot;
+
+public class EndPoint {
+    public static void main(String[] args) {
+        ApiContextInitializer.init(); // Инициализируем апи
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new TestBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}

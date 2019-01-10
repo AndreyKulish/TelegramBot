f5663fa5e0f5aab4182b4eceb7a58b4d8a8eddd1
Andrey Kylish <a.kylish@andersenlab.com>
2019-01-10 18:21:44 +0200

    1_try

--- src/main/java/test/bot/TestBot.java
+++ src/main/java/test/bot/TestBot.java
@@ -6,19 +6,13 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TestBot extends TelegramLongPollingBot {
-
+    
     @Override
     public void onUpdateReceived(Update update) {
 // We check if the update has a message and the message has text
+        System.out.println("wkek" + update.getMessage().getChatId());
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText() + " FROM BOT");
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+            filter(update);
         }
         System.out.println("kek" + update.getMessage().getChatId());
     }
@@ -33,5 +27,42 @@ public class TestBot extends TelegramLongPollingBot {
         return "534762565:AAFo70xBCRJrMk_Um2CH0qisydUA2gZ0Zks";
     }
 
+
+    private void filter(Update update){
+        String actor = update.getMessage().getFrom().getFirstName();
+        System.out.println(update.getMessage().getText());
+        if(actor.equals("Sergey")){
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId());
+            message.setText("Сергей ленивая жопка, хули ты не учишься говнюк сранный??!!");
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if(actor.equals("Andrey")){
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId());
+            message.setText("Андрюша молодец!");
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if(actor.equals("Yuri")){
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId());
+            message.setText("Юрасик вообще бомба - молодец!");
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 }
--- src/main/java/test/core/EndPoint.java
+++ src/main/java/test/core/EndPoint.java
@@ -4,8 +4,8 @@ package test.core;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import test.bot.TestBot;
-import test.bot.ThreadBotWorker;
+import test.bot.TestBot;/*
+import test.bot.ThreadBotWorker;*/
 
 public class EndPoint {
     public static void main(String[] args) {
@@ -14,8 +14,8 @@ public class EndPoint {
         try {
             TestBot testBot = new TestBot();
             botapi.registerBot(testBot);
-            Thread thread = new Thread(new ThreadBotWorker(testBot));
-            thread.start();
+            /*Thread thread = new Thread(new ThreadBotWorker(testBot));
+            thread.start();*/
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
